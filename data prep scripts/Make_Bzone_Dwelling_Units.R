7df3f45acbcb046654a9ec0da1654352c69f5e26
Eric Englin <Eric.Englin@dot.gov>
2020-11-30 16:36:11 -0500

    clean up make_bzone_dwelling_unit script. Almost finished at this point.

--- data prep scripts/Make_Bzone_Dwelling_Units.R
+++ data prep scripts/Make_Bzone_Dwelling_Units.R
@@ -52,7 +52,7 @@ dwell_units_raw <- get_acs(geography = "tract", table = "S1101",
                            state = "VA", county = counties, geometry = FALSE)
 
 # Download with geography
-dwell_units_geo <- get_acs(geography = "tract", table = "S1101",
+dwell_units_raw <- get_acs(geography = "tract", table = "S1101",
                            state = "VA", county = counties, geometry = TRUE)
 
 
@@ -60,53 +60,47 @@ dwell_units_geo <- get_acs(geography = "tract", table = "S1101",
 dwell_units_016 <- dwell_units_raw %>% filter(variable=="S1101_C01_016") # one unit total households
 dwell_units_017 <- dwell_units_raw %>% filter(variable=="S1101_C01_017") # two unit total households
 dwell_units_018 <- dwell_units_raw %>% filter(variable=="S1101_C01_018") # mobile home total households
+dwell_units_total <- dwell_units_raw %>% filter(variable=="S1101_C01_001") # mobile home total households
+
 
 # Make sure all have the same GEOID
 # The tract is value 6 - 9 in the census GEOID
 identical(dwell_units_016$GEOID, dwell_units_018$GEOID) &
-  identical(dwell_units_016$GEOID, dwell_units_017$GEOID)
+  identical(dwell_units_016$GEOID, dwell_units_017$GEOID) &
+  identical(dwell_units_016$GEOID, dwell_units_total$GEOID)
 
-
-#recreate data with geography
-dwell_units_geo_016 <- dwell_units_geo %>% filter(variable=="S1101_C01_016") # one unit total households
-dwell_units_geo_017 <- dwell_units_geo %>% filter(variable=="S1101_C01_017") # two unit total households
-dwell_units_geo_018 <- dwell_units_geo %>% filter(variable=="S1101_C01_018") # mobile home total households
-
-# Make sure all have the same GEOID
-# The tract is value 6 - 9 in the census GEOID
-identical(dwell_units_016$GEOID, dwell_units_018$GEOID) &
-  identical(dwell_units_016$GEOID, dwell_units_017$GEOID)
-
-dwell_units_geo <- dwell_units_geo_016 %>% mutate (one_unit = dwell_units_geo_016$estimate, #bring in the 3 census variables
-                                      mobile_home = dwell_units_geo_018$estimate,
-                                      two_unit = dwell_units_geo_017$estimate) %>%
-  mutate(SFDU = one_unit + mobile_home, #change census variables to VisionEval variables
-         MFDU = two_unit,
+dwell_units_geo <- dwell_units_016 %>% mutate (one_unit = dwell_units_016$estimate, #bring in the 3 census variables
+                                                   mobile_home = dwell_units_018$estimate,
+                                                   two_unit = dwell_units_017$estimate,
+                                                  total = dwell_units_total$estimate) %>%
+  mutate(SFDU = (one_unit + mobile_home)*total/100, #change census variables to VisionEval variables
+         MFDU = two_unit*total/100,
          GQDU = 1) %>%
   mutate(Geo = substr(GEOID, 6, 9)) %>%
-  select("Geo", "SFDU", "MFDU", "GQDU", "GEOID") #%>% #filter dataframe columns
+  select("Geo", "SFDU", "MFDU", "GQDU", "GEOID") %>% #filter dataframe columns
+  replace(is.na(.), 0)
 
 # Save the tract geography to temp
 dwell_units_geo_sp = as_Spatial(dwell_units_geo)
 
 rgdal::writeOGR(obj = dwell_units_geo_sp,
                 dsn = temp,
-                layer = 'bzone_tract_v2',
+                layer = 'census_tract',
                 driver = 'ESRI Shapefile')
 
 
 # save tract-based temporary file
-
 dwell_units_no_geo<-st_set_geometry(dwell_units_geo, NULL) #remove geometry field
 write.csv(dwell_units_no_geo, file.path(temp, 'bzone_dwelling_units_by_tract.csv'), row.names = FALSE) #save as csv
 
 
 
 ################################################################################################
-########### In Progress After this Point #######################################################
-
 
 TAZ_geometry <- st_read(file.path(input, "FFXsubzone/FFX_Subzone.shp")) #load TAZ dataset
+TAZ_geometry_no_geo<-st_set_geometry(TAZ_geometry, NULL) #remove geometry field
+write.csv(TAZ_geometry_no_geo, file.path(temp, 'TAZ_geometry.csv'), row.names = FALSE) #save as csv
+
 TAZ_geometry_sp <- as(TAZ_geometry, Class = "Spatial")
 
 #change all to USGS project for continuity
@@ -115,61 +109,53 @@ TAZ_geometry_sp_newproj <- spTransform(TAZ_geometry_sp, CRS = proj.USGS)
 dwell_units_geo_sp_newproj <- spTransform(dwell_units_geo_sp, CRS = proj.USGS)
 
 ###############################################################################
-#Method 2: gIntersection
 library("rgeos")
 
 #gIntersection
 #gIntersection(TAZ_geometry2_sp, results_geo_sp, byid = TRUE)
 gI <- gIntersection(TAZ_geometry_sp_newproj, dwell_units_geo_sp_newproj, byid=TRUE, drop_lower_td=TRUE)
-#plot(gI, add=TRUE, border="red", lwd=3) #plot goes over previous plot
-
-
-int<-gIntersection(TAZ_geometry2_sp, results_geo_sp,byid=T) # Multiple polygons per province
-n<-names(int)
+n<-names(gI)
 n<-data.frame(t(data.frame(strsplit(n," ",fixed=TRUE))))
+colnames(n)[1:2]<-c("id_TAZ","id_tract")
+n$area<-sapply(gI@polygons, function(x) x@area)
+a<-data.frame(id=row.names(TAZ_geometry_sp_newproj), TAZ_N = TAZ_geometry_sp_newproj$TAZ_N)
+df<-merge(n,a,by.x = "id_TAZ", by.y = "id", all.x=TRUE)
 
-colnames(n)[1:2]<-c("id","id2")
-n$area<-sapply(int@polygons, function(x) x@area)
-a<-data.frame(id=row.names(TAZ_geometry2_sp),total.area=TAZ_geometry2_sp$SHAPE_AREA)
-df<-merge(n,a,all.x=TRUE)
+side_df <- df %>%   group_by(id_tract)%>%
+                    summarise(n = n(),
+                              shape_area = sum(area))
+            
+df<-merge(df,side_df,by =  "id_tract", all.x=TRUE)
 
-df$share.area<-df$area/df$total.area*100
-df$id <- as.integer(df$id)
-df$id2 <- as.integer(df$id2)
+dwell_units_geo$id_tract <- seq.int(nrow(dwell_units_geo))
+df2<- merge(df, dwell_units_geo, by = "id_tract", by.y = "id_tract", all.x=TRUE)
 
+df2 <- df2 %>% mutate(share.area = area/shape_area,
+                   SFDU_this_area = SFDU * share.area,
+                   MFDU_this_area = MFDU * share.area,
+                   GQDU_this_area = GQDU * share.area) 
 
+df2_no_geo <-df2 %>% select(-'geometry')
+write.csv(df2_no_geo, file.path(temp, 'temp_df2.csv'), row.names = FALSE) #save as csv
 
-#Create TAZ vs Census Tract comparison plots
-par(mfrow = c(1, 2))
-plot(st_geometry(TAZ_geometry),
-     main = 'TAZ')
-plot(st_geometry(dwell_units_geo),
-     main = 'Tract')
+df3 <- df2 %>% group_by(TAZ_N)%>%
+              summarise(n = n(),
+                        SFDU = sum(SFDU_this_area),
+                        GQDU = sum(GQDU_this_area),
+                        MFDU = sum(MFDU_this_area))
+    
 
-par(mfrow = c(1, 2))
-plot(TAZ_geometry['SUB_POP15'],
-     main = 'TAZ - population 2015')
-plot(result_geo['SFDU'],
-     main = 'Tract - SFDU')
-
-
-#Clean the TAZ data, join to Census Tract data
-TAZ_geometry2 <- st_transform(TAZ_geometry, 4269)
-TAZ_geometry2_sp = as_Spatial(TAZ_geometry2)
+write.csv(df3, file.path(temp, 'temp_df.csv'), row.names = FALSE) #save as csv
 
 
-###############################################################################
-#Method 1: Simple Spatial Join
-spatial_joined_df <- st_join(TAZ_geometry2, result_geo) #join TAZ and Census Tract data
+TAZ_geometry_reordered <- TAZ_geometry[order(TAZ_geometry$TAZ_N),]
+df3_geo <-st_set_geometry(df3, TAZ_geometry_reordered$geometry) 
+plot(df3_geo['GQDU'],
+     main = 'TAZ - MFDU')
 
-#Aggregate data to TAZ level
-spatial_joined_df2 <- spatial_joined_df %>% select("Geo","SFDU","MFDU","GQDU","GEOID","TAZ") %>%
-  group_by(TAZ)%>%
-  summarise(n = n(),
-            SFDU = mean(SFDU),
-            MFDU = mean(MFDU),
-            GQDU = mean(GQDU))
-#Plot results to check SFDU at TAZ level
-par(mfrow = c(1, 1))
-plot(result_geo['SFDU'], main = "Census - SFDU")
-plot(spatial_joined_df2['SFDU'], main = "TAZ - SFDU")
+df3_geo_sp = as_Spatial(df3_geo)
+
+rgdal::writeOGR(obj = df3_geo_sp,
+                dsn = temp,
+                layer = 'TAZ',
+                driver = 'ESRI Shapefile')
