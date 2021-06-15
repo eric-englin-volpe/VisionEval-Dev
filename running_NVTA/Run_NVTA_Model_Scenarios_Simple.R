5f9344adcc5f4446b49cff0bcb3c79b979e08689
dflynn-volpe <daniel.flynn@dot.gov>
2021-06-15 14:00:05 -0400

    Updates for more general installation location and more user feedback

--- running_NVTA/Run_NVTA_Model_Scenarios_Simple.R
+++ running_NVTA/Run_NVTA_Model_Scenarios_Simple.R
@@ -12,41 +12,42 @@ files <- list.dirs("models/scenario_inputs",full.names = FALSE,
 
 #iterate through the A-P scenarios
 for (item in files){
-  print(item)
+  cat('Preparing Scenario', item, '\n\n')
   name <- paste0("models/scenario_inputs/",item)
   models <- list.files(name, full.names = FALSE, recursive = FALSE,
                       pattern ="[2-9]")
   
   # run through each case excluding the 1 case
   for (case in models){
-    print(case)
+    cat('\tPreparing level', case, '\t')
     
     # create the model name i.e. VERSPM_NVTA_A2
     base <- openModel('VERSPM_NVTA')
     modelName <- paste0('VERSPM_NVTA_',item,case)
     print(modelName)
     
-    locName <- paste0(name,"/",case)
-    print(locName)
+    locName <- paste0(name,"/", case)
+    cat('Saving to', locName)
     toChange <- list.files(locName,full.names = FALSE)
-    print(toChange)
+    cat('Modifying file(s):\n', paste(toChange, collapse = '\n'), '\n')
     
     #create the model run folder
-    if(!dir.exists(paste0("/models",modelName))){
-    runningModel <- base$copy(modelName)
+    if(!dir.exists(file.path(ve.runtime, "models", modelName))){
+      runningModel <- base$copy(modelName)
     }
     
-    #prepare to copy over user changed input files
+    # prepare to copy over user changed input files
+    # Use ve.runtime to locate where VisionEval is installed
     
     for (f in toChange){
-      from <- paste0("C:/VisionEval/",locName,"/",f)
+      from <- file.path(ve.runtime, locName, f)
       print(from)
-      to <- paste0("C:/VisionEval/models/",modelName,"/inputs/",f)
+      to <- file.path(ve.runtime, 'models', modelName, "inputs", f)
       print(to)
       
-      #verify the to and from files are different
-      stopifnot(md5sum(from)!=md5sum(to))
-      file.copy(from,to, overwrite =TRUE)
+      # verify the to and from files are different
+      stopifnot(md5sum(from) != md5sum(to))
+      file.copy(from, to, overwrite = TRUE)
     }
   
 
